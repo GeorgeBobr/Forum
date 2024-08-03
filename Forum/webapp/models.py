6ac60d85c8c18f430ce5eda4288a7e19e0cf89f7
Georg <georgestoneix@gmail.com>
2024-08-03 18:04:49 +0600

    Добавил подсчет

--- Forum/webapp/models.py
+++ Forum/webapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    answers_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
+
+class Reply(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='replies', verbose_name='Тема')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='replies', verbose_name='Автор')
+    content = models.TextField(verbose_name='Содержание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    class Meta:
+        ordering = ['created_at']
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+
+    def __str__(self):
+        return f'{self.author} - {self.topic}'
